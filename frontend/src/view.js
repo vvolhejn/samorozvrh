79a76dd0764f815f8521566e0a0ead0b3209fb91
Václav <vaclav.volhejn@gmail.com>
2018-02-16 11:54:51 +0100

    Fix schedule timestamps not matching event times

--- frontend/src/view.js
+++ frontend/src/view.js
@@ -28,7 +28,7 @@ export function renderSchedule(groupsByDay) {
         }
     })
     var templateData = {
-        timestamps: ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00"],
+        timestamps: ["07:00", "08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00"],
         groupsByDay: templateGroupsByDay,
     }
 
